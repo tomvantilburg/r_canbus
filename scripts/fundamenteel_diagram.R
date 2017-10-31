0b6cfae3dc56c74cbc0a6bcb7184284416c713cd
Tom van Tilburg <tomt.van.tilburg@gmail.com>
2017-10-31 09:58:40 +0100

    added plumber service

--- scripts/fundamenteel_diagram.R
+++ scripts/fundamenteel_diagram.R
@@ -1,5 +1,6 @@
 require("RPostgreSQL")
 require("ggplot2")
+require("hexbin")
 
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, dbname = "research",
@@ -9,7 +10,7 @@ df_postgres <- dbGetQuery(con, "
 WITH elements AS (
 	SELECT mst_id,json_array_elements(characteristics) elem
 	FROM ndw.mst_points
-	WHERE mst_id = 'GRT02_MORO_1373_2'
+	WHERE mst_id = 'RWS01_MONIBAS_0161hrl0173ra'
 )
 ,indices AS (
 	SELECT 
@@ -34,14 +35,24 @@ data AS (
 	AND a.values[b.index] > 0
 )
 SELECT 
-	date, location, date_part('hour',date) as tod,
+	date, 
+  location,
+  CASE 
+    WHEN date_part('dow',date) = 5 THEN 'friday'
+    ELSE 'not friday'
+  END as day,
+  CASE 
+    WHEN date_part('hour',date) < 6 OR date_part('hour',date) >= 20 THEN 'night'
+    WHEN date_part('hour',date) >= 6 AND date_part('hour',date) < 10 THEN 'morning'
+    WHEN date_part('hour',date) >= 10 AND date_part('hour',date) < 17 THEN 'day'
+    WHEN date_part('hour',date) >= 17 AND date_part('hour',date) < 20 THEN 'evening'
+    END as tod,
 	avg(speedvalue) as speed_avg, count(speedvalue) as speedvalues,
 	sum(flowvalue) as flow_sum, count(flowvalue) as flowvalues
 FROM data a
 GROUP BY a.location, a.date
 ")
-ggplot(df_postgres,aes(flow_sum,speed_avg, colour = factor(tod))) + geom_point(shape="o") + theme_bw()
-
+ggplot(df_postgres,aes(flow_sum,speed_avg, colour = factor(tod))) + geom_point() + theme_bw()
 
 hist(df_postgres$speed_avg, breaks = seq(min(df_postgres$speed_avg, na.rm = T),max(df_postgres$speed_avg, na.rm=T),1))
 hist(df_postgres$flow_sum, breaks = seq(min(df_postgres$flow_sum, na.rm = T),max(df_postgres$flow_sum, na.rm=T),1))
