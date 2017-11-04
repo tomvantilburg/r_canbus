1d598c0df6acbfc1285a1f6013335fd0fa216d8d
joepke <j.keijsers@gmail.com>
2017-11-04 22:10:18 +0100

    LAG naar volgende i.p.v. vorige waarde

--- scripts/geom_dist_vs_calc_dist.r
+++ scripts/geom_dist_vs_calc_dist.r
@@ -12,8 +12,8 @@ m <- dbGetQuery(con, "WITH distance AS (
                 value as speed, 
                 time, 
                 ST_Transform(geom, 28992) as geom, 
-                DATE_PART('second', time - Lag(time, 1) OVER(ORDER BY vin, time)) dt,
-                (value + Lag(value, 1) OVER(ORDER BY vin, time)) / 2 avspeed
+                DATE_PART('second', time - LAG(time, -1) OVER(ORDER BY vin, time)) dt,
+                (value + LAG(value, -1) OVER(ORDER BY vin, time)) / 2 avspeed
                 
                 FROM canbus.data_2017
                 WHERE signalid = 191 AND vin = 'fe41054ca9a55158862afd2b06aafe74eb31900f64ba89e070adaa52555b261b' 
@@ -31,7 +31,7 @@ ORDER by min(time))
 --SELECT * from distance2;
 
 SELECT 	b.time, b.x as calc_dist,
-ST_DISTANCE(b.geom, (Lag(b.geom, 1) OVER (ORDER BY b.time))) as geom_dist
+ST_DISTANCE(b.geom, (LAG(b.geom, -1) OVER (ORDER BY b.time))) as geom_dist
 
 FROM distance2 b ORDER BY b.time;
                     ")
@@ -39,5 +39,7 @@ FROM distance2 b ORDER BY b.time;
 
 dbDisconnect(con)
 
-plot(m$geom_dist, m$calc_dist,xlim=c(0,1000), ylim=c(0,1000))
-lines(x = c(0,10000), y=c(0,10000), lty=2)
+plot(m$calc_dist, m$geom_dist, xlim=c(0,500), ylim=c(0,500), 
+     xlab='distance from speed and time (m)',
+     ylab='distance between GPS positions (m)')
+abline(coef=c(1,1), lty=2)
