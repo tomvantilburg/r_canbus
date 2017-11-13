b2005381a73fb0001683dc5b4c84c95df2c3a0ef
Tom van Tilburg <tomt.van.tilburg@gmail.com>
2017-11-13 13:53:04 +0100

    refactoring and cleaning

--- scripts/heatmaps.r
+++ scripts/heatmaps.r
@@ -1,3 +1,9 @@
+###
+# Several heatmaps on canbus data. 
+# authors: TomT
+###
+
+
 require("RPostgreSQL")
 require("ggplot2")
 require("hexbin")
@@ -6,51 +12,6 @@ drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, dbname = "research",
                  host = "localhost", port = 5433,
                  user = "postgres")
-df_postgres <- dbGetQuery(con, "
-  SELECT count(DISTINCT vin) vehicles, date_trunc('day',time) date
-  FROM canbus.data_2017 
-  GROUP BY date_trunc('day',time)
-  ORDER BY date_trunc('day',time)
-")
-#Conclusion: before 19-march id's are switched more then 1x a day
-ggplot(df_postgres,aes(date,vehicles)) + geom_line(color="steelblue") + theme_bw()
-
-
-#Types of measurements
-df_postgres <- dbGetQuery(con, "
-  SELECT b.name, count(a.signalid) n
-  FROM canbus.data_2017 a
-  INNER JOIN canbus.signals b
-  ON a.signalid = b.signalid
-  WHERE time > '2017-03-19'
-  GROUP BY b.name
-  ORDER BY b.name
-")
-ggplot(df_postgres, aes(x=name, y=n)) +
-  geom_bar(stat='identity',fill='steelblue') +
-  coord_flip() + theme_minimal()
-
-#histogram of number of speed measurements per geom
-df_postgres <- dbGetQuery(con, "
-  WITH grouped AS (
-    SELECT geom,count(geom) n
-    FROM canbus.data_2017 
-    WHERE signalid = 191 
-    AND value > 10
-    AND time > '2017-03-19'
-    GROUP BY geom
-  )
-  SELECT n, count(geom) freq
-  FROM grouped
-  GROUP BY n
-  ORDER BY n
-")
-ggplot(data=df_postgres, aes(x=n, y=freq)) +
-  geom_bar(stat="identity", fill="steelblue") +
-  labs(title="Number of speedmeasurements per GPS point", 
-       x="#-measurements", y = "occurance") +
-  theme_minimal()
-
 
 library(ggmap)
 nl <- c(left = 3.5, bottom = 50.5, right = 7, top = 53.5)
@@ -59,11 +20,11 @@ ggmap(map)
 
 #Heatmap of speedmeasurements
 df_postgres <- dbGetQuery(con, "
-  SELECT DISTINCT round(ST_X(geom)::numeric,2) lon, round(ST_Y(geom)::numeric,2) lat
-  FROM canbus.data_2017 
-  WHERE time > '2017-03-19' AND signalid = '191' AND value > 10
-  
-")
+                          SELECT DISTINCT round(ST_X(geom)::numeric,2) lon, round(ST_Y(geom)::numeric,2) lat
+                          FROM canbus.data_2017 
+                          WHERE time > '2017-03-19' AND signalid = '191' AND value > 10
+                          
+                          ")
 ggplot(df_postgres,aes(date,vehicles)) + geom_line() + theme_bw()
 qmplot(lon, lat, data = df_postgres, geom = "blank", zoom = 7, maptype = "toner-background", darken = .7, legend = "topright") +
   stat_density_2d(aes(fill = ..level..), geom = "polygon", alpha = .3, color = NA) +
@@ -71,11 +32,11 @@ qmplot(lon, lat, data = df_postgres, geom = "blank", zoom = 7, maptype = "toner-
 
 #Heatmap of foglight measurements
 df_postgres <- dbGetQuery(con, "
-  SELECT DISTINCT round(ST_X(geom)::numeric,2) lon, round(ST_Y(geom)::numeric,2) lat
-  FROM canbus.data_2017 
-  WHERE time > '2017-03-19' AND (signalid = '100' OR signalid = '170')
-  
-")
+                          SELECT DISTINCT round(ST_X(geom)::numeric,2) lon, round(ST_Y(geom)::numeric,2) lat
+                          FROM canbus.data_2017 
+                          WHERE time > '2017-03-19' AND (signalid = '100' OR signalid = '170')
+                          
+                          ")
 ggplot(df_postgres,aes(date,vehicles)) + geom_line() + theme_bw()
 qmplot(lon, lat, data = df_postgres, geom = "blank", zoom = 7, maptype = "toner-background", darken = .7, legend = "topright") +
   stat_density_2d(aes(fill = ..level..), geom = "polygon", alpha = .3, color = NA) +
