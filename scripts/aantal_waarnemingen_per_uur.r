656fa8436c92d35a4f7d9f10d62f8f5efe54f41d
joepke <joep.keijsers@geodan.nl>
2018-03-15 22:46:53 +0100

    Geschikt maken voor R-studio op de server

--- scripts/aantal_waarnemingen_per_uur.r
+++ scripts/aantal_waarnemingen_per_uur.r
@@ -3,20 +3,22 @@
 require("RPostgreSQL")
 drv <- dbDriver("PostgreSQL")
 con <- dbConnect(drv, dbname = "research",
-                 host = "localhost", port = 5433,
+                 host = "metis", port = 5433,
                  user = "postgres")
 
 
 df <- dbGetQuery(con, "
-                 SELECT value speed, vin as auto, signalid, date_part('dow', time) as day, date_part('hour', time) as time from canbus.data_2017
+                 SELECT value speed, 
+                 vin as auto, 
+                 signalid, 
+                 date_part('dow', time) as day, 
+                 date_part('hour', time) as time
+                 FROM canbus.data_2017 
+                 WHERE time > '2017-04-01'
                  ")
+dbDisconnect(con)
 
-## 'jet.colors' is "as in Matlab"
-## (and hurting the eyes by over-saturation)
-jet.colors <-
-  (c("#00007F", "blue", "#007FFF", "cyan",
-                     "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"))
-
+col = c('grey', 'green', 'black', 'black', 'black', 'blue', 'grey')
 x=0:23
 plot(NULL,
      xlim=c(0,24),
@@ -25,14 +27,14 @@ plot(NULL,
      ylab = 'aantal waarnemingen',
      type='o', las=1)
 
-col = c('grey',rep('black',5),
-                    'grey')
-dagen=c('zo', 'ma', 'di', 'wo', 'do', 'vr', 'za')
+#col = c('grey',rep('black',5),
+#                    'grey')
+dagen=c('zo', 'ma', '', '', '', 'vr', 'za')
 for (day in 0:6){
   df2 = df[df$day == day,]
   a = hist(df2$time,breaks = x, plot=F)
-  lines(a$mids,a$counts,col=col[day+1])
-  text(x = a$mids[9], y = a$counts[9], labels = dagen[day+1], pos=3, offset = c(0,0.5), cex = 0.6)
+  lines(a$mids,a$counts,col=col[day+1], lwd=1)
+  #text(x = a$mids[9], y = a$counts[9], labels = dagen[day+1], pos=3, offset = c(0,0.5), cex = 0.8)
 }
 legend(x='topleft', legend=c('weekend','doordeweeks'),col=c('grey', 'black'), lty=1, inset=0.02)
 
